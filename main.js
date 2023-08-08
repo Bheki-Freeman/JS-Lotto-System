34c3f66f527640325c19ece5ef0ef28fbb16160c
Bheki Freeman <bhekisisadlamini05@gmail.com>
2023-08-08 13:00:32 +0200

    The players main file

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log(`Eventually running javascript for a lotto machine`);
+//The player class
+class Player{
+    line = '--------------------------------'
+    //for logins
+    username = '' 
+    password = ''
+    //
+    
+    constructor(firstname, lastname, phone, gender, email, residential){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone
+        this.gender = gender
+        this.email = email
+        this.residential = residential
+    }
+
+    greetPlayer() {
+
+        console.log(`Hello ${this.firstname}`)
+        
+    }
+}
+
+const player = new Player('Freeman', 'Dlamini', 76594512, 'm', 'mail@google.com', 'mbabane mncitsini')
+player.greetPlayer()
+//Since we are using javascript, we won't be doing much of a console program, we just need to incoporate the html and css
